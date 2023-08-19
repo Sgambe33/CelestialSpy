1d38ce94070bd62b723f3405f14ae142908c133b
Sgambe33 <80386324+Sgambe33@users.noreply.github.com>
2023-08-19 19:00:24 +0200

    Uppercase satellites categories

--- src/main/java/com/example/javafxtest/Globals.java
+++ src/main/java/com/example/javafxtest/Globals.java
@@ -6,9 +6,9 @@ public class Globals {
 
     public static final HashMap<String, String> SATELLITES_URL = new HashMap<>();
     static {
-        SATELLITES_URL.put("stations", "https://celestrak.org/NORAD/elements/gp.php?GROUP=stations&FORMAT=tle");
-        SATELLITES_URL.put("weather", "https://celestrak.org/NORAD/elements/gp.php?GROUP=weather&FORMAT=tle");
-        SATELLITES_URL.put("geo", "https://celestrak.org/NORAD/elements/gp.php?GROUP=geo&FORMAT=tle");
-        SATELLITES_URL.put("gnss", "https://celestrak.org/NORAD/elements/gp.php?GROUP=gnss&FORMAT=tle");
+        SATELLITES_URL.put("STATIONS", "https://celestrak.org/NORAD/elements/gp.php?GROUP=stations&FORMAT=tle");
+        SATELLITES_URL.put("WEATHER", "https://celestrak.org/NORAD/elements/gp.php?GROUP=weather&FORMAT=tle");
+        SATELLITES_URL.put("GEO", "https://celestrak.org/NORAD/elements/gp.php?GROUP=geo&FORMAT=tle");
+        SATELLITES_URL.put("GNSS", "https://celestrak.org/NORAD/elements/gp.php?GROUP=gnss&FORMAT=tle");
     }
 }
