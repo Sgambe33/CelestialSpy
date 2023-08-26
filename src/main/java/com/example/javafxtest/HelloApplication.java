9ff7e00f2e901871f9cd034d0bb71dd3129c0de9
Sgambe33 <80386324+Sgambe33@users.noreply.github.com>
2023-08-26 19:45:48 +0200

    Moved Orekit initializiation instructions

--- src/main/java/com/example/javafxtest/HelloApplication.java
+++ src/main/java/com/example/javafxtest/HelloApplication.java
@@ -11,32 +11,46 @@ import com.esri.arcgisruntime.ArcGISRuntimeEnvironment;
 import com.esri.arcgisruntime.mapping.ArcGISMap;
 import com.esri.arcgisruntime.mapping.BasemapStyle;
 import com.esri.arcgisruntime.mapping.view.MapView;
+import org.orekit.data.DataContext;
+import org.orekit.data.DataProvidersManager;
+import org.orekit.data.DirectoryCrawler;
+import org.orekit.data.ZipJarCrawler;
+
+import java.io.File;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
-    private MapView mapView;
-
     @Override
     public void start(Stage stage) throws IOException {
 
+        //===================ARCGIS INIT====================//
+
         ArcGISRuntimeEnvironment.setApiKey(yourApiKey);
         ArcGISRuntimeEnvironment.setLicense(licenseKey);
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+
+        //===================OREKIT INIT====================//
+        File orekitData = new File("E:\\GITHUB_REPOS\\JavaFXTest\\orekit-data-master.zip");
+        DataProvidersManager manager = DataContext.getDefault().getDataProvidersManager();
+        manager.addProvider(new ZipJarCrawler(orekitData));
+
+
+
+        //===================JavaFX INIT====================//
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("test2.fxml"));
         //Apply the CSS stylesheet to the scene
         Scene scene = new Scene(fxmlLoader.load(), 640, 375);
         scene.getStylesheets().add(getClass().getResource("stylesheet.css").toExternalForm());
-        stage.setTitle("ARCGIS MAP");
+        stage.setTitle("CelestialSpy");
         stage.setScene(scene);
+        stage.setMinHeight(500);
+        stage.setMinWidth(700);
         stage.show();
     }
-    @Override
-    public void stop() {
-        if (mapView != null) {
-            mapView.dispose();
-        }
-    }
 
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws IOException {
+        Utils utils = new Utils();
+        utils.updateTLEs();
         launch();
     }
 }
