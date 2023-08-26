e5bb5eb34496d3f751704ba471c7853a15d2af5a
Sgambe33 <80386324+Sgambe33@users.noreply.github.com>
2023-08-26 19:47:37 +0200

    Switched to hashmap instead of array to read TLEs

--- src/main/java/com/example/javafxtest/Utils.java
+++ src/main/java/com/example/javafxtest/Utils.java
@@ -4,6 +4,7 @@ import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Date;
+import java.util.HashMap;
 
 public class Utils {
     private String readFromInputStream(InputStream inputStream)
@@ -37,13 +38,15 @@ public class Utils {
     }
 
     public boolean updateTLEs() throws IOException {
-        String TLEpath = System.getProperty("user.home") + File.separatorChar + "My Documents" + File.separatorChar + "CelestialSpy" + File.separatorChar + "TLEs";
+        String TLEpath = System.getProperty("user.home") + File.separatorChar + "Documents" + File.separatorChar + "CelestialSpy" + File.separatorChar + "TLEs";
         File TLEdir = new File(TLEpath);
         if (!TLEdir.exists()) {
+            System.out.println("Creating TLE directory");
             TLEdir.mkdirs();
         }
         File latestUpdate = new File(TLEpath + File.separatorChar + "latestUpdate.txt");
         if (!latestUpdate.exists()) {
+            System.out.println("Creating latestUpdate file");
             latestUpdate.createNewFile();
             FileWriter fileWriter = new FileWriter(latestUpdate);
             fileWriter.write("0");
@@ -52,13 +55,14 @@ public class Utils {
         InputStream inputStream = new FileInputStream(latestUpdate);
         String latestUpdateString = readFromInputStream(inputStream);
         Date latestUpdateDate = new Date(Long.parseLong(latestUpdateString.replace("\n", "")));
-        if ((latestUpdateDate.getTime() - new Date().getTime()) > 604800000) {
+        if ((new Date().getTime() - latestUpdateDate.getTime()) > 604800000) {
             System.out.println("TLEs are older than 7 days, updating...");
             for (String key : Globals.SATELLITES_URL.keySet()) {
                 String data = httpGetCall(Globals.SATELLITES_URL.get(key));
                 //Dump data to file
                 File file = new File(TLEpath + File.separatorChar + key + ".txt");
                 if (!file.exists()) {
+                    System.out.println("Creating TLE file for " + key);
                     file.createNewFile();
                 }
                 FileWriter fileWriter = new FileWriter(file);
@@ -70,6 +74,55 @@ public class Utils {
             fileWriter.close();
             return true;
         }
+        System.out.println("TLEs are up to date");
         return false;
+    }
+
+    public Satellite[] readTLEs(String category){
+        File TLEpath = new File(System.getProperty("user.home") + File.separatorChar + "Documents" + File.separatorChar + "CelestialSpy" + File.separatorChar + "TLEs" + File.separatorChar + category + ".txt");
+        if (!TLEpath.exists()) {
+            System.out.println("TLE file not found");
+            return null;
+        }
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(TLEpath));
+            String line;
+            int i = 0;
+            //Could be optimized by counting the number of lines in the file and then creating the array or even better an ArrayList
+            Satellite[] satellites = new Satellite[1000];
+            while ((line = br.readLine()) != null) {
+                String name = line;
+                String line1 = br.readLine();
+                String line2 = br.readLine();
+                satellites[i] = new Satellite(name.trim(), line1, line2);
+                i++;
+            }
+            return satellites;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+    public HashMap<String, Satellite> readTLEsH(String category){
+        File TLEpath = new File(System.getProperty("user.home") + File.separatorChar + "Documents" + File.separatorChar + "CelestialSpy" + File.separatorChar + "TLEs" + File.separatorChar + category + ".txt");
+        if (!TLEpath.exists()) {
+            System.out.println("TLE file not found");
+            return null;
+        }
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(TLEpath));
+            String line;
+            HashMap<String, Satellite> satellites = new HashMap<String, Satellite>();
+            while ((line = br.readLine()) != null) {
+                String name = line;
+                String line1 = br.readLine();
+                String line2 = br.readLine();
+                satellites.put(name.trim(), new Satellite(name.trim(), line1, line2));
+            }
+            return satellites;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
