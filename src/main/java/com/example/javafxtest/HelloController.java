ada9244baac8316f32c528e22626b847b8aa77dd
Sgambe33 <80386324+Sgambe33@users.noreply.github.com>
2023-08-26 19:46:17 +0200

    Brute logic to track satellite on checkbox click with threads

--- src/main/java/com/example/javafxtest/HelloController.java
+++ src/main/java/com/example/javafxtest/HelloController.java
@@ -1,38 +1,31 @@
 package com.example.javafxtest;
 
-import com.esri.arcgisruntime.geometry.Point;
-import com.esri.arcgisruntime.geometry.SpatialReferences;
+import com.esri.arcgisruntime.geometry.*;
 import com.esri.arcgisruntime.mapping.ArcGISMap;
 import com.esri.arcgisruntime.mapping.BasemapStyle;
 import com.esri.arcgisruntime.mapping.Viewpoint;
+import com.esri.arcgisruntime.mapping.labeling.SimpleLabelExpression;
 import com.esri.arcgisruntime.mapping.view.Graphic;
 import com.esri.arcgisruntime.mapping.view.GraphicsOverlay;
 import com.esri.arcgisruntime.mapping.view.MapView;
 
+import com.esri.arcgisruntime.mapping.view.ViewLabelProperties;
 import com.esri.arcgisruntime.symbology.SimpleLineSymbol;
 import com.esri.arcgisruntime.symbology.SimpleMarkerSymbol;
+import com.esri.arcgisruntime.symbology.Symbol;
+import com.esri.arcgisruntime.symbology.TextSymbol;
 import javafx.fxml.FXML;
+import javafx.geometry.Point2D;
 import javafx.scene.control.*;
 import javafx.scene.paint.Color;
-import org.orekit.bodies.GeodeticPoint;
-import org.orekit.bodies.OneAxisEllipsoid;
-import org.orekit.data.DataContext;
-import org.orekit.data.DataProvidersManager;
-import org.orekit.data.DirectoryCrawler;
-import org.orekit.frames.FramesFactory;
-import org.orekit.propagation.SpacecraftState;
-import org.orekit.propagation.analytical.tle.TLE;
-import org.orekit.propagation.analytical.tle.TLEPropagator;
-import org.orekit.time.AbsoluteDate;
-import org.orekit.time.TimeScalesFactory;
-import org.orekit.utils.Constants;
-import org.orekit.utils.IERSConventions;
-import org.hipparchus.geometry.euclidean.threed.Vector3D;
 
-import java.io.File;
 import java.io.IOException;
-import java.time.LocalDateTime;
-import java.util.Date;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import static com.example.javafxtest.Globals.blueOutlineSymbol;
+import static com.example.javafxtest.Globals.simpleMarkerSymbol;
 
 public class HelloController {
     @FXML
@@ -40,48 +33,115 @@ public class HelloController {
     @FXML
     private MenuBar menuBar;
     @FXML
-    private ScrollPane scrollPane;
+    private ListView<CheckBox> listView;
+    @FXML
+    private ComboBox<String> satCategory;
+    private Utils utils = new Utils();
+    private HashMap<String, LatLongLocation> currentlyTrackedSats = new HashMap<String, LatLongLocation>();
+    private HashMap<String, Satellite> satellites = new HashMap<String, Satellite>();
+    private final GraphicsOverlay graphicsOverlay = new GraphicsOverlay();
+    private Satellite[] sats;
+
 
 
     public void initialize() throws IOException {
-        //Add checkbox with label to scrollpane
+        //====================SATELLITE CATEGORIES LOADING====================//
+        List<String> categories = new ArrayList<String>(Globals.SATELLITES_URL.keySet());
+        satCategory.getItems().addAll(categories);
 
-        Utils utils = new Utils();
-        utils.updateTLEs();
-        ArcGISMap map = new ArcGISMap(BasemapStyle.OSM_STANDARD);
+        //====================ARCGIS INIT====================//
+        ArcGISMap map = new ArcGISMap(BasemapStyle.ARCGIS_IMAGERY);
         mappa.setMap(map);
         mappa.setViewpoint(new Viewpoint(34.02700, -118.80543, 1000000000));
-
-        // create a graphics overlay and add it to the map view
-        GraphicsOverlay graphicsOverlay = new GraphicsOverlay();
         mappa.getGraphicsOverlays().add(graphicsOverlay);
-
-        File orekitData = new File("C:\\Users\\Cosimo\\Desktop\\orekit-data-master");
-        DataProvidersManager manager = DataContext.getDefault().getDataProvidersManager();
-        manager.addProvider(new DirectoryCrawler(orekitData));
-        OrekitGetLLATest orekitGetLLATest = new OrekitGetLLATest();
-        orekitGetLLATest.setUp();
-        orekitGetLLATest.setUpBeforeEach();
-
-        //===================================//
-        Point point = new Point(orekitGetLLATest.testGetLongitudeFromTLEUsingOrekit(), orekitGetLLATest.testGetLatitudeFromTLEUsingOrekit(), SpatialReferences.getWgs84());
-        // create an opaque orange point symbol with a opaque blue outline symbol
-        SimpleMarkerSymbol simpleMarkerSymbol =
-                new SimpleMarkerSymbol(SimpleMarkerSymbol.Style.CIRCLE, Color.ORANGE, 10);
-        SimpleLineSymbol blueOutlineSymbol =
-                new SimpleLineSymbol(SimpleLineSymbol.Style.SOLID, Color.BLUE, 2);
         simpleMarkerSymbol.setOutline(blueOutlineSymbol);
 
-        // create a graphic with the point geometry and symbol
-        Graphic pointGraphic = new Graphic(point, simpleMarkerSymbol);
+        mappa.setOnMouseClicked(e -> {
+            Point point = mappa.screenToLocation(new Point2D(e.getX(), e.getY()));
+            point = (Point) GeometryEngine.project(point, SpatialReferences.getWgs84());
+            System.out.println(point);
+            Graphic pointGraphic = new Graphic(point, simpleMarkerSymbol);
+            graphicsOverlay.getGraphics().add(pointGraphic);
 
-        // add the point graphic to the graphics overlay
-        graphicsOverlay.getGraphics().add(pointGraphic);
+            //Find nearest satellite
+            double minDistance = Double.MAX_VALUE;
+            String nearestSat = "";
+            for (Satellite sat : sats) {
+                if(sat == null){
+                    break;
+                }
+                double distance = sat.getCurrentPosition().distanceTo(point);
+                if(distance < minDistance){
+                    minDistance = distance;
+                    nearestSat = sat.name;
+                }
+            }
+            System.out.println("Nearest satellite: " + nearestSat + " at " + minDistance + " km");
 
-        //update the point graphic's geometry
-        point = new Point(22,22, SpatialReferences.getWgs84());
-        pointGraphic.setGeometry(point);
+        });
+    }
 
-        utils.updateTLEs();
+    public void handleComboBoxAction(){
+        System.out.println(satCategory.getValue());
+        sats = utils.readTLEs(satCategory.getValue());
+        List<CheckBox> checkBoxes = new ArrayList<CheckBox>();
+        for (Satellite sat : sats) {
+            if(sat == null){
+                break;
+            }
+            System.out.println(sat.name);
+            //TODO: add better checkbox listener
+            CheckBox test = new CheckBox(sat.name);
+            test.setOnAction((event) -> {
+                if(test.isSelected()){
+                    System.out.println(test.getText() + " got selected");
+                    //Find satellite in sats array
+                    for (Satellite sat1 : sats) {
+                        if (sat1.name.equals(test.getText())) {
+                            Thread one = new Thread() {
+                                public void run() {
+                                    Point point = new Point(0,0, SpatialReferences.getWgs84());
+                                    Graphic pointGraphic = new Graphic(point, simpleMarkerSymbol);
+                                    graphicsOverlay.getGraphics().add(pointGraphic);
+
+                                    Graphic label = new Graphic(point, new TextSymbol(15, sat1.name, Color.BLACK, TextSymbol.HorizontalAlignment.CENTER, TextSymbol.VerticalAlignment.TOP));
+                                    label.setSelected(true);
+                                    graphicsOverlay.getGraphics().add(label);
+
+                                    while(true){
+                                        try {
+                                            Thread.sleep(1000);
+
+                                            System.out.println(sat1.getCurrentPosition());
+                                            point = new Point(sat1.getCurrentPosition().getLongitude(),sat1.getCurrentPosition().getLatitude(), SpatialReferences.getWgs84());
+                                            pointGraphic.setGeometry(point);
+                                            label.setGeometry(point);
+                                        } catch(InterruptedException v) {
+                                            System.out.println(v);
+                                        }
+                                    }
+
+                                }
+                            };
+
+                            one.start();
+                            System.out.println(sat1.getCurrentPosition());
+                        }
+                    }
+
+                }
+            });
+            checkBoxes.add(test);
+        }
+        //Add ALL checkboxes
+        listView.getItems().clear();
+        listView.getItems().addAll(checkBoxes);
+    }
+
+    public void stop() {
+        if (mappa != null) {
+            mappa.dispose();
+        }
     }
 }
+
